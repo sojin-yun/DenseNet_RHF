1565ad3c6acb487c8a9d2e50938445049d24cedb
ykykyk112 <ykykyk112@naver.com>
2021-09-02 16:05:20 +0900

    2021.09.02 * make mini_imagenet dataset

--- utils/mini_imagenet.py
+++ utils/mini_imagenet.py
@@ -6,6 +6,8 @@ from utils.random_seed import fix_randomness
 
 fix_randomness(42)
 
+print(os.getcwd())
+
 total_classes = os.listdir('/home/NAS_mount/sjlee/ILSVRC/Data/CLS-LOC/train/')
 sampled_classes = random.sample(total_classes, 100)
 sampled_classes.sort()
