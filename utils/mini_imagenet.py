7194fd7a5f56022e59400c17db9b9f3580083610
ykykyk112 <ykykyk112@naver.com>
2021-09-02 16:06:00 +0900

    2021.09.02 * make mini_imagenet dataset

--- utils/mini_imagenet.py
+++ utils/mini_imagenet.py
@@ -1,12 +1,10 @@
 import os, sys
-sys.path.append('/home/sjlee/recovering_hfc')
-sys.path.append('/home/sjlee/recovering_hfc/DenseNet_RHF')
 import random
 from utils.random_seed import fix_randomness
 
 fix_randomness(42)
 
-print(os.getcwd())
+sys.path.append(os.getcwd())
 
 total_classes = os.listdir('/home/NAS_mount/sjlee/ILSVRC/Data/CLS-LOC/train/')
 sampled_classes = random.sample(total_classes, 100)
