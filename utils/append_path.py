fe51e02da1f7750372b454f7d67c8dabab9762a0
ykykyk112 <ykykyk112@naver.com>
2021-09-02 16:10:20 +0900

    2021.09.02 * make mini_imagenet dataset

--- utils/append_path.py
+++
@@ -1,4 +0,0 @@
-import sys, os
-
-def add_sys_path(main_cwd : str) :
-    print(os.listdir(main_cwd))
--- utils/mini_imagenet.py
+++ utils/mini_imagenet.py
@@ -1,11 +1,9 @@
 import os, sys
+sys.path.append(os.getcwd()+'utils/')
 import random
 from utils.random_seed import fix_randomness
-from utils.append_path import add_sys_path
 
 fix_randomness(42)
-
-add_sys_path(os.getcwd())
 
 total_classes = os.listdir('/home/NAS_mount/sjlee/ILSVRC/Data/CLS-LOC/train/')
 sampled_classes = random.sample(total_classes, 100)
