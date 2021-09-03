28cbfd7453efe49b353acc4237e9ef43d6878340
ykykyk112 <ykykyk112@naver.com>
2021-09-04 01:31:25 +0900

    2021.09.03 * Build pipeline to training network * Test on ResNet18 model

--- model/resnet.py
+++ model/resnet.py
@@ -121,7 +121,7 @@ class ResNet_ensemble(nn.Module):
         self.boundary_loss = nn.CrossEntropyLoss()
         self.ensemble_loss = nn.CrossEntropyLoss()
         #self.scheduler = StepLR(self.optimizer, step_size=15, gamma=0.5)
-        self.scheduler = MultiStepLR(self.optimizer, milestones=[60, 120, 160, 200], gamma = 0.2)
+        self.scheduler = MultiStepLR(self.optimizer, milestones=[40, 80, 100, 120, 140], gamma = 0.2)
 
         # weight initialization
         for idx, m in enumerate(self.modules()):
