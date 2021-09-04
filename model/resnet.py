d6c44182e685f779441eb0238be154cea87e949f
ykykyk112 <ykykyk112@naver.com>
2021-09-05 01:15:50 +0900

    2021.09.03 * Build pipeline to training network * Test on ResNet18 model

--- model/resnet.py
+++ model/resnet.py
@@ -116,7 +116,8 @@ class ResNet_ensemble(nn.Module):
 
         self.ensemble_relu = nn.ReLU(inplace=True)
 
-        self.optimizer = optim.SGD(self.parameters(), lr = 1e-1, momentum = 0.9, weight_decay=1e-3)
+        self.optimizer = optim.SGD(self.parameters(), lr = 1e-2, momentum = 0.9, weight_decay=0.0015)
+        #self.optimizer = optim.SGD(self.parameters(), lr = 1e-1, momentum = 0.9, weight_decay=1e-3)
         self.loss = nn.CrossEntropyLoss()
         self.boundary_loss = nn.CrossEntropyLoss()
         self.ensemble_loss = nn.CrossEntropyLoss()
