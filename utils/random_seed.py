25c6df96969b31ea843c768fcc54dcac3a9b2847
hannah <hannah.kim7120@gmail.com>
2021-09-13 13:39:28 +0900

    2021.09.13 * Push in server * Fix random seed to get reproducibility

--- utils/random_seed.py
+++ utils/random_seed.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def Fix_Randomness(in_number) :
     seed_number = in_number
-    torch.use_deterministic_algorithms(True)
+    #torch.use_deterministic_algorithms(True)
     torch.manual_seed(seed_number)
     torch.cuda.manual_seed(seed_number)
     torch.cuda.manual_seed_all(seed_number)
