14c1c2b9c6ffbda5793d351336dad79f938c1f41
ykykyk112 <ykykyk112@naver.com>
2021-10-12 20:37:13 +0900

    2021.10.09 * Test ImageNet-C dataset

--- model/baseline/resnet_deeper.py
+++ model/baseline/resnet_deeper.py
@@ -144,7 +144,6 @@ class ResNet_deeper(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.identity(x)
         x = self.layer4(x)
 
         x = self.cam_relu(x)
