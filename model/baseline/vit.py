681de2d259df2d4f052588c45694a64914cef6c8
ykykyk112 <ykykyk112@naver.com>
2021-12-13 15:17:17 +0900

    2021.12.13 * Add pretrained vision transformer model to baseline

--- model/baseline/vit.py
+++ model/baseline/vit.py
@@ -1,9 +1,24 @@
 from pytorch_pretrained_vit import ViT
 import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
 def VisionTransformer(type : str, pretrained : bool = True) :
     model = ViT(type, pretrained)
+
+    optimizer = optim.SGD(model.parameters(), lr = 0.01, momentum = 0.9, weight_decay=0.0015)
+    setattr(model, 'optimizer', optimizer)
+
+    loss = nn.CrossEntropyLoss()
+    setattr(model, 'loss', loss)
+    
+    scheduler = StepLR(model.optimizer, step_size=15, gamma=0.5)
+    setattr(model, 'scheduler', scheduler)
+
+    modified_fc = nn.Linear(768, 100, bias = True)
+    nn.init.xavier_normal_(modified_fc.weight)
+
     for n, m in model.named_modules() :
         if n == 'fc' :
-            setattr(model, n, nn.Linear(768, 100, bias = True))
+            setattr(model, n, modified_fc)
     return model
