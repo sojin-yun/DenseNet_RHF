cfbf1a4397dfeb103f0bebd357797a79c79ff033
ykykyk112 <ykykyk112@naver.com>
2021-12-13 15:02:09 +0900

    2021.12.13 * Add pretrained vision transformer model to baseline

--- model/baseline/vit.py
+++ model/baseline/vit.py
@@ -1,4 +1,9 @@
 from pytorch_pretrained_vit import ViT
+import torch.nn as nn
 
 def VisionTransformer(type : str, pretrained : bool = True) :
-    return ViT(type, pretrained)
+    model = ViT(type, pretrained)
+    for n, m in model.named_modeles() :
+        if n == 'fc' :
+            setattr(model, n, nn.Linear(768, 100, bias = True))
+    return model
