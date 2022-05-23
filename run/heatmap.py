9779973f383f4f6a4ed21ee88abee83ff10c2d2d
ykykyk112 <ykykyk112@naver.com>
2022-05-23 13:00:08 +0900

    2022.05.23 * Get a heatmap of switched version of RenseNet

--- run/heatmap.py
+++ run/heatmap.py
@@ -227,13 +227,13 @@ class RunGradCAM() :
             ax4 = fig.add_subplot(2, 3, 5)
             ax4.imshow(mask_np, cmap = 'gray')
             ax4.imshow((baseline_ret * 255.).astype('uint8'), cmap = 'jet', alpha = 0.4)
-            ax4.set_title('120th epoch / Acc : 74.00%', fontsize = 12)
+            ax4.set_title('120th epoch / Acc : 71.42%', fontsize = 12)
             ax4.axis('off')
 
             ax5 = fig.add_subplot(2, 3, 6)
             ax5.imshow(mask_np, cmap = 'gray')
             ax5.imshow((ensemble_ret * 255.).astype('uint8'), cmap = 'jet', alpha = 0.4)
-            ax5.set_title('114th epoch / Acc : 78.57%', fontsize = 12)
+            ax5.set_title('105th epoch / Acc : 73.41%', fontsize = 12)
             ax5.axis('off')
 
             plt.savefig(figsave_path+'/{0}.png'.format(str(idx)))
