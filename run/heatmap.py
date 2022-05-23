c34ac8e9f54352cd004706489fc7d7160b5197d7
ykykyk112 <ykykyk112@naver.com>
2022-05-23 12:56:03 +0900

    2022.05.23 * Get a heatmap of switched version of RenseNet

--- run/heatmap.py
+++ run/heatmap.py
@@ -47,8 +47,8 @@ class RunGradCAM() :
         self.ensemble_model.load_state_dict(ensemble_params['state_dict'])
 
         # [mini_imagenet, cifar100]
-        self.hooked_layer = {'baseline' : {'vgg16' : [44, 43], 'vgg19' : [53, 32], 'resnet50' : [152, 152], 'resnet101' : [288, 287], 'resnet152' : [424, 423], 'densenet121' : [492, 491], 'densenet169' : [684, 683], 'densenet201' : [812, 811], 'rensenet' : [180, 180], 'srensenet' : [180, 180]},
-                             'ensemble' : {'vgg16' : [142, 102], 'vgg19' : [141, 111], 'resnet50' : [201, 198], 'resnet101' : [322, 321], 'resnet152' : [458, 457], 'densenet121' : [528, 518], 'densenet169' : [717, 716], 'densenet201' : [845, 844], 'rensenet' : [225, 225], 'srensenet' : [180, 180]}}
+        self.hooked_layer = {'baseline' : {'vgg16' : [44, 43], 'vgg19' : [53, 32], 'resnet50' : [152, 152], 'resnet101' : [288, 287], 'resnet152' : [424, 423], 'densenet121' : [492, 491], 'densenet169' : [684, 683], 'densenet201' : [812, 811], 'rensenet' : [180, 180], 'srensenet' : [10, 10]},
+                             'ensemble' : {'vgg16' : [142, 102], 'vgg19' : [141, 111], 'resnet50' : [201, 198], 'resnet101' : [322, 321], 'resnet152' : [458, 457], 'densenet121' : [528, 518], 'densenet169' : [717, 716], 'densenet201' : [845, 844], 'rensenet' : [225, 225], 'srensenet' : [10, 10]}}
         idx = 1 if self.args['data'] == 'cifar100' else 0
 
         self.baseline_cam = GradCAM(model = self.baseline_model, hooked_layer = self.hooked_layer['baseline'][self.args['model']][idx], device = self.device, ensemble = False)
