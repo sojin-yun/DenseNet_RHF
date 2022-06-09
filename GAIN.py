9f128c550db30dfc690d7d54aff59dddd7146930
ykykyk112 <ykykyk112@naver.com>
2022-06-09 15:33:19 +0900

    2022.06.09 * Commit for Project

---
+++ GAIN.py
@@ -0,0 +1,99 @@
+from re import S
+from tkinter import image_names
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GAIN(nn.Module):
+    def __init__(self,  device, model, hooked_layer):
+        super(GAIN, self).__init__()
+        self.device = device
+        self.model = model
+
+        self.forward_result = None
+        self.backward_result = None
+
+        self.loss = nn.CrossEntropyLoss()
+
+        #total_loss = loss_cl + alpha*loss_am
+        self.alpha = 0.01
+        
+        #for softmask
+        self.sigma = 0.7
+        self.omega = 10
+
+        #hook
+        list(self.model.modules())[hooked_layer].register_forward_hook(self.forward_hook)
+        print('Hook on {0}'.format(list(self.model.modules())[hooked_layer]))
+        
+    def forward_hook(self, _, input_image, output):
+        input_image[0].register_hook(self.hook)
+        self.forward_result = torch.squeeze(output) 
+
+
+    def hook(self, grad):   #backward hook
+        self.backward_result = grad     
+        
+    
+
+    def attention_map_forward(self, images, labels):
+        #generate gradcam
+        output_cl = self.model(images)
+        Sc = 0.
+        _, predictions = torch.max(output_cl, 1)
+        for idx in range(len(labels)): #for each image batch
+            _, pred = torch.max(output_cl[idx], dim = 0) #_: max value, pred : the index of the max value
+            # predictions.append(pred.item())
+            Sc += output_cl[idx, pred]  #loss에 crossentropy가 아닌, y^c자체를 줌 (y^c를 feature map으로 미분하기 위해)
+        
+        #back propagation
+        Sc.backward(retain_graph=True)
+        self.model.zero_grad()
+
+        fl = F.relu(self.forward_result)
+        loss_cl = self.loss(output_cl, labels)
+        pool = nn.AdaptiveAvgPool2d((1, 1))
+        weights = pool(self.backward_result)
+        Ac = torch.mul(fl, weights).sum(dim=1, keepdim=True)
+        Ac = F.relu(Ac)
+        Ac = F.upsample_bilinear(Ac, size=images.size()[2:])
+        return output_cl, loss_cl, Ac, predictions
+
+    def softmask(self, Ac, images):
+        Ac_min = Ac.min()
+        Ac_max = Ac.max()
+        try:
+            scaled_ac = (Ac - Ac_min) / (Ac_max - Ac_min)
+        except ZeroDivisionError:
+            scaled_ac = torch.zeros_like(Ac)
+        mask = torch.sigmoid(torch.exp(self.omega * (scaled_ac - self.sigma)))
+        masked_image = images - images * mask
+        return masked_image
+
+    
+    def forward(self, images, labels):
+        output_cl, loss_cl, Ac, preds = self.attention_map_forward(images, labels)
+
+        I_star = self.softmask(Ac, images) 
+        output_am = self.model(I_star)
+        self.model.zero_grad()
+        # ImageNet
+        loss_am = torch.sum(torch.sigmoid(output_am), dim=(0,1), keepdim=False)
+        loss_am /= len(labels)
+        # Lung
+        #loss_am = torch.sum(F.softmax(output_am), dim=(0,1), keepdim=False)
+        #loss_am /= len(labels)
+        total_loss = loss_cl + self.alpha * loss_am
+        return total_loss, loss_cl, loss_am, Ac, preds
+
+        
+
+
+
+
+
+        
+
+
+
+
---
+++ project.py
@@ -0,0 +1,95 @@
+import sys
+import os
+
+from utils.select_model import Select_Model
+from utils.random_seed import Fix_Randomness
+from utils.parse_args import Parsing_Args
+from utils.load_data import CustomDataLoader
+from model.baseline.resnet_deeper import BasicBlock_deeper, ResNet_deeper
+from model.baseline.densenet import DenseNet, Transition, Bottleneck
+from GAIN import GAIN
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+def drive(args) :
+
+    Fix_Randomness(42)
+
+    flags = Parsing_Args(args)
+    n_device = 'cuda:{}'.format(flags['device']) if flags['device'] != 'cpu' else 'cpu'
+    device = torch.device(n_device)
+    epoch = flags['epoch']
+    data_loader = CustomDataLoader(flags)()
+    train_loader, valid_loader = data_loader
+
+    abs_path = '/home/NAS_mount/sjlee/RHF' if flags['server'] else '.'
+
+    # Model Selection
+    if flags['model'] == 'resnet50' :
+        cam_model = ResNet_deeper(BasicBlock_deeper, [3, 4, 6, 3], 100, None, low_resolution = False)
+        gain_model = GAIN(device, cam_model, 152)
+
+    elif flags['model'] == 'densenet121' :
+        cam_model = DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=32, num_class = 100, low_resolution = False)
+        gain_model = GAIN(device, cam_model, 492)
+
+    # Use Pretrained Weights
+    print('Training Start')
+    for i in range(epoch) :
+
+            train_loss, valid_loss = 0.0, 0.0
+            train_acc, valid_acc = 0.0, 0.0
+
+            print('------------[Epoch:{}]-------------'.format(i+1))
+            gain_model.train()
+
+            n_train_batchs = len(train_loader)
+            n_valid_batchs = len(valid_loader)
+            batch_size = flags['batch_size']
+
+            for train_iter, (samples) in enumerate(tqdm(train_loader, desc="{:17s}".format('Training State'), mininterval=0.01)) :
+
+                
+                if flags['mask'] : train_data, train_target, _ = samples
+                else : train_data, train_target = samples
+
+                if device != None : train_data, train_target = train_data.to(device), train_target.to(device)
+                
+                total_loss, loss_cl, loss_am, Ac, pred = gain_model(train_data, train_target)
+
+                total_loss.backward()
+
+                gain_model.model.optimizer.step()
+                
+                train_acc = (torch.sum(pred == train_target.data).item()*(100.0 / batch_size))
+                print(total_loss)
+
+            with torch.enable_grad() :
+
+                gain_model.eval()
+
+                for valid_iter, (v_samples) in enumerate(tqdm(valid_loader, desc="{:17s}".format('Evaluation State'), mininterval=0.01)) :
+
+                    if flags['mask'] : valid_data, valid_target, _ = v_samples
+                    else : valid_data, valid_target = v_samples
+
+                    if device != None : valid_data, valid_target = valid_data.to(device), valid_target.to(device)
+
+                    gain_model.model.optimizer.zero_grad()
+
+                    v_total_loss, v_loss_cl, v_loss_am, Ac, preds = gain_model(valid_data, valid_target)
+
+            training_result = 'epoch.{0:3d} \t train_ac : {1:.4f}% \t  valid_ac : {2:.4f}% \t lr : {3:.6f}\n'.format(i+1, avg_train_acc, avg_valid_acc, curr_lr)
+
+            avg_train_acc = train_acc/n_train_batchs
+            avg_valid_acc = valid_acc/n_valid_batchs
+            avg_tarin_loss = train_loss/n_train_batchs
+            avg_valid_loss = valid_loss/n_valid_batchs
+
+            curr_lr = gain_model.model.optimizer.param_groups[0]['lr']
+
+            gain_model.model.scheduler.step()
+
+if __name__ == '__main__' :
+    drive(sys.argv[1:])
