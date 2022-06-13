aa74d46852db22dec1512e7eb574468e37c22a33
ykykyk112 <ykykyk112@naver.com>
2022-06-13 13:15:21 +0900

    2022.06.13 * Get a heatmap of switched version of RenseNet

--- GAIN.py
+++ GAIN.py
@@ -83,7 +83,7 @@ class GAIN(nn.Module):
         # loss_am = torch.sum(torch.sigmoid(output_am), dim=(0,1), keepdim=False)
         # loss_am /= len(labels)
         # Lung
-        loss_am = torch.sum(F.softmax(output_am), dim=(0,1), keepdim=False)
+        loss_am = torch.sum(torch.softmax(output_am), dim=(0,1), keepdim=False)
         loss_am /= len(labels)
         total_loss = loss_cl + self.alpha * loss_am
         return total_loss, loss_cl, loss_am, Ac, preds
